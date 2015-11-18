a29241bcae059d3c833a66f9d450f902c821e67a
rgehi <rachitagehi@Rachitas-MacBook-Pro.local>
2015-11-18 12:55:28 -0800

    Created Table 11-18-15
    Basic table has been created.

--- js/datasource.js
+++ js/datasource.js
@@ -11,6 +11,27 @@ MyDataSource.prototype = {
     }
 };
 
+function createTable() {
+    var no = ["1", "2", "3", "4"];
+    var names = ["Roger", "Steve", "Mike", "Charlie"];
+    var countries = ["USA", "Canada", "UK", "Germany"];
+    var cname = ["abc.com", "xyz.com", "123.com", "ABB.com"];
+
+    //Test that if all array values were null then String "no data to be displayed" shows up
+    //var names, countries, no, cname;
+    if (no == null && names == null & countries == null && cname == null)
+        $("#result").append("No data to be displayed.");
+    else {
+        for (i = 0; i < no.length; i++) {
+            var $formrow = '<tr><td>' + no[i] + '</td><td>' + names[i] + '</td><td>' + countries[i] + '</td><td>' + cname[i] + '</td></tr>';
+            // $('.myTable').append($formrow);
+            $('#result').append($formrow);
+        }
+    }
+}
+
+
+
 $(document).ready(function() {
 
     $("#get_user").click(function() {
@@ -24,6 +45,10 @@ $(document).ready(function() {
         $("#result").empty();
         $("#result").append(id);
         $("#result").append(name);
+
+        createTable();
+
+
 
     });
 
