a5d120bd0ef360fc50155df378020c742dfedc35
amyqn <nguyen00amy@gmail.com>
2016-01-26 00:47:41 -0800

    Added x to breadcrumbs and ability to delete breadcrumbs

--- js/BreadCrumbs.js
+++ js/BreadCrumbs.js
@@ -11,22 +11,25 @@ Merquery.BreadCrumbs = {
     //Forms the breadcrumbs
     formBreadcrumb: function () {
         if ($('#breadcrumbs').empty()){
-            $('#breadcrumbs').append("<a>initalSearch </a>/ ");
+            //initialSearch should be the one from the left navigation bar
+            $('#breadcrumbs').append("<a><em>&times;</em>initalSearch</a>");
         }
         var crumbs = Merquery.BreadCrumbs.crumbs;
         for (var prop in crumbs) {
             if (crumbs.hasOwnProperty(prop)) {
-                var test = $("<li></li>");
-                var link = $("<a></a>");
+                var link = $("<a><em>&times;</em></a>");
                 link.attr("href", "javascript:void(0)");
                 link.append(prop + " : " + crumbs[prop] + " ");
                 link.data("value", crumbs[prop]);
                 link.data("queryField", prop);
-                test.append(link);
-                $('#breadcrumbs').append(test);
+                $('#breadcrumbs').append(link);
 
                 link.click(function () {
-                    Merquery.Queries.standardQuery($(this).data("value"), $(this).data("queryField"), selText);
+                    //Merquery.Queries.standardQuery($(this).data("value"), $(this).data("queryField"), selText);
+                    if ($(this).data("queryField") in crumbs) {
+                        delete crumbs[$(this).data("queryField")];
+                    }
+                    Merquery.Queries.breadCrumbQuery(crumbs, selText);
                 });
             }
         }
