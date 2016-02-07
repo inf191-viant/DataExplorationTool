1706000c32de4570833c0da92b09521b72ac88f8
amyqn <nguyen00amy@gmail.com>
2016-02-07 11:14:59 -0800

    Added initial search fields to breadcrumbs; adjusted breadcrumb type

--- js/BreadCrumbs.js
+++ js/BreadCrumbs.js
@@ -5,40 +5,53 @@ if(typeof Merquery == "undefined" ) {
 Merquery.BreadCrumbs = {
     crumbs: [],
 
-    clearBreadCrumbs: function() {
+    clearBreadcrumbs: function() {
         Merquery.BreadCrumbs.crumbs = [];
     },
-    //Forms the breadcrumbs
-    formBreadcrumb: function () {
-        if ($('#breadcrumbs').empty()){
-            //initialSearch should be the one from the left navigation bar
-            $('#breadcrumbs').append("<a><em>&times;</em>initalSearch</a>");
-        }
-        var crumbs = Merquery.BreadCrumbs.crumbs;
-        for (var prop in crumbs) {
-            if (crumbs.hasOwnProperty(prop)) {
-                var link = $("<a><em>&times;</em></a>");
-                link.attr("href", "javascript:void(0)");
-                link.append(prop + " : " + crumbs[prop] + " ");
-                link.data("value", crumbs[prop]);
-                link.data("queryField", prop);
-                $('#breadcrumbs').append(link);
 
-                link.click(function () {
-                    if ($(this).data("queryField") in crumbs) {
-                        delete crumbs[$(this).data("queryField")];
+    // Forms and displays the breadcrumbs
+    displayBreadcrumbs: function() {
+        var crumbs = Merquery.BreadCrumbs.crumbs;
+        var temporaryCrumbs = [];
+        for (var i =0; i < crumbs.length; i++) {
+            var link = $("<a><em>&times;</em></a>");
+            link.attr("href", "javascript:void(0)");
+            link.append(crumbs[i].queryField + " : " + crumbs[i].input);
+            link.data("value", crumbs[i].input);
+            link.data("queryField", crumbs[i].queryField);
+            link.data("type", crumbs[i].querytype);
+            $('#breadcrumbs').append(link);
+
+            link.click(function () {
+                for (var i =0; i<crumbs.length; i++)
+                {
+                    if (crumbs[i].queryField != $(this).data("queryField"))
+                    {
+                        temporaryCrumbs.push(crumbs[i]);
                     }
-                    Merquery.Queries.breadCrumbQuery(crumbs);
-                });
-            }
+                }
+                Merquery.BreadCrumbs.clearBreadcrumbs();
+                crumbs = Merquery.BreadCrumbs.crumbs;
+                for (var i = 0; i<temporaryCrumbs.length; i++) {
+                    crumbs.push(temporaryCrumbs[i]);
+                }
+                Merquery.Queries.runQuery(crumbs);
+            });
         }
     },
 
-    //Returns the total count of rows based on the filters from breadcrumbs
-    totalRowCount:function () {
-        return rowCount;
+
+
+    // checkForDuplicateInputs checks whether the value is already in the breadcrumbs
+    // before adding to the breadcrumbs
+    checkForDuplicateInputs: function(userValues) {
+        var crumbs = Merquery.BreadCrumbs.crumbs;
+        for (var i =0; i < crumbs.length; i++) {
+            if (crumbs[i].queryField == userValues.queryField) {
+                return false;
+            }
+        }
+        return true;
     }
 
-
 }
-
