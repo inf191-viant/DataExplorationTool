f1280b41fb5a92c28a28b07312071e233d5e3cf6
amyqn <nguyen00amy@gmail.com>
2016-02-18 19:39:58 -0800

    Changed runquery; query is now formed in formquery

--- js/queries.js
+++ js/queries.js
@@ -131,74 +131,12 @@ Merquery.Queries = {
         $('#export').empty();
         $('#result').empty();
 
-        var queryString;
-        var endString;
-        var inputs;
-
-        queryString =  "SELECT Demographics.firstname, Demographics.lastname, Demographics.emailmd5, Demographics.birthdate, Demographics.gender, Demographics.city, Demographics.ethnicity," +
-            " Demographics.sexorient,Demographics.marital, Demographics.children, Address.Address1, Address.Address2, Address.City," +
-            " Address.State, Address.Zip, Campaign.advertiser_id, Campaign.advertiser_name, Campaign.campaign_id, Campaign.campaign_name," +
-            " Campaign.impression_count, Campaign.click_count, Campaign.conversion_count, Behavior.id, Behavior.name, Behavior.last_seen," +
-            " Behavior.count, Device.device_id, Device.device_name, Device.device_type_id, Device.device_type_name, Device.operating_system," +
-            " Device.last_seen, Purchase.advertiser_id, Purchase.last_sales_amount, Purchase.last_purchase_date, Purchase.ltv_online," +
-            " Purchase.ltv_offline " +
-
-
-            "FROM (SELECT firstname, lastname, emailmd5, birthdate,gender, city, ethnicity, sexorient, marital, children FROM [formal-cascade-571:uci.demo_info] " +
-            "where emailmd5 is not null ) AS Demographics " +
-
-            "LEFT JOIN EACH (SELECT emailmd5, Address1, Address2, City, State, Zip FROM [formal-cascade-571:uci.address_info]) "+
-            "AS Address ON Address.emailmd5 = Demographics.emailmd5 " +
-
-            "LEFT JOIN EACH (SELECT emailmd5, advertiser_id, advertiser_name, campaign_id, campaign_name, impression_count, " +
-            "click_count, conversion_count FROM [formal-cascade-571:uci.campaign_info]) AS Campaign ON Campaign.emailmd5 = Demographics.emailmd5 " +
-
-            "LEFT JOIN EACH (SELECT emailmd5, id, name, last_seen, count FROM [formal-cascade-571:uci.behavior_info]) " +
-            "AS Behavior ON Behavior.emailmd5 = Demographics.emailmd5 " +
-
-            "LEFT JOIN EACH (SELECT emailmd5, device_id, device_name, device_type_id, device_type_name, operating_system, " +
-            "last_seen FROM [formal-cascade-571:uci.device_info]) AS Device ON Device.emailmd5 = Demographics.emailmd5 " +
-
-            "LEFT JOIN EACH (SELECT emailmd5, advertiser_id, last_sales_amount, last_purchase_date, ltv_online, " +
-            "ltv_offline FROM [formal-cascade-571:uci.purchase_info]) AS Purchase ON Purchase.emailmd5 = Demographics.emailmd5 " +
-
-            'where ';
-
-        for(var i=0; i<userInputs.length; i++){
-            var formattedFieldName;
-            if(i ==0){
-                formattedFieldName = Merquery.TableAndFieldsFormatter.formatFieldName(userInputs[i].queryField)
-                inputs =  'lower(' + formattedFieldName + ') ' + ' like "%' + userInputs[i].input + '%"';
-            }
-            else if(i>0){
-                inputs = inputs + ' AND ';
-                formattedFieldName = Merquery.TableAndFieldsFormatter.formatFieldName(userInputs[i].queryField)
-                if(userInputs[i].querytype == 'STRING')
-                    inputs =  inputs + 'lower(' + formattedFieldName + ') ' + ' like "%' + userInputs[i].input + '%"';
-                else
-                    inputs =  inputs + formattedFieldName + ' = ' + userInputs[i].input;
-            }
-        }
-
-        endString = " GROUP each BY Demographics.firstname, Demographics.lastname, Demographics.emailmd5, Demographics.birthdate, Demographics.gender, " +
-            "Demographics.city, Demographics.ethnicity, Demographics.sexorient, Demographics.marital, " +
-            "Demographics.children, Address.Address1, Address.Address2, Address.City, Address.State, " +
-            "Address.Zip, Campaign.advertiser_id, Campaign.advertiser_name, Campaign.campaign_id, "+
-            "Campaign.campaign_name, Campaign.impression_count,Campaign.click_count, Campaign.conversion_count, " +
-            "Behavior.id, Behavior.name, Behavior.last_seen, Behavior.count, " +
-            "Device.device_id, Device.device_name, Device.device_type_id, Device.device_type_name, " +
-            "Device.operating_system, Device.last_seen, " +
-            " Purchase.advertiser_id, Purchase.last_sales_amount, Purchase.last_purchase_date, Purchase.ltv_online, Purchase.ltv_offline "
-            + 'Limit ' + "50" + ';';
-
-
-        Merquery.Util.log(queryString + inputs + endString);
 
         var request = gapi.client.bigquery.jobs.query({
 
             'projectId': Merquery.Constants.project_id,
             'timeoutMs': '30000',
-            'query': queryString + inputs + endString
+            'query': Merquery.queryMaker.formQuery()
 
         });
         request.execute(Merquery.renderResults);
